aeaea435c634f4eb090d41dd11413142b81acdee
Madhav Aryal <074bct520.madhav@pcampus.edu.np>
2020-08-21 20:10:27 +0545

    gatsby-node changed generatesource map false

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,11 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  if (getConfig().mode === "production") {
+    actions.setWebpackConfig({
+      devtool: false,
+    });
+  }
+};
